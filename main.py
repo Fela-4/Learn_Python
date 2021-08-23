1e7e176dd4a9992075aced42a1cc19f0d9a8ce54
kipling <kip.crossing@gmail.com>
2021-08-23 18:38:53 +1000

    Add class

--- main.py
+++ main.py
@@ -2,6 +2,30 @@
 import math
 from pprint import pprint
 
+
+
+def increments_by_x(ls, x):
+    return [l + x for l in ls]
+
+
+l = [1,2,3,4,5]
+
+print(l.reverse())
+print(increments_by_x(l, 5))
+
+
+class Incrementer:
+    def __init__(self):
+        pass
+
+    def increments_by_x(self, ls, x):
+        return [l + x for l in ls]
+
+
+inc = Incrementer()
+
+print(inc.increments_by_x(l, 4))
+
 if __name__ == "__main__":
     print(math.pi)
     print("Running script...")
