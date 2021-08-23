0f131bb2b4189c08abb775f0ef546374a49a325d
kipling <kip.crossing@gmail.com>
2021-08-23 18:14:28 +1000

    pretty print a dict

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # This is a test script
 import math
-
-
+from pprint import pprint
 
 if __name__ == "__main__":
     print(math.pi)
@@ -10,3 +9,9 @@ if __name__ == "__main__":
     b = 5
     res = 10 / 5
     print(res)
+    u_duct = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+    }
+    pprint(u_duct)
