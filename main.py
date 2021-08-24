df2c5e66359da96aecbbb9b53814bab9ff8a62c7
Fela <felachhkchhk4@gmail.com>
2021-08-25 00:29:17 +0530

    Rock_Paper_Scissors_Game Function added

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 # This is a test script
 import math
 from pprint import pprint
@@ -15,3 +18,21 @@ if __name__ == "__main__":
         "three": 3,
     }
     pprint(u_duct)
+
+#Rock Paper Scissors Game
+
+def is_win(user, computer)
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+
+
+def play()
+    user= input("What is your choice: 'p' for paper, 'r' for rock and 's' for scissors\n")
+    comp = random.choice(['r','p','s']) 
+
+    if user == computer:
+        return "It's a tie"
+    if is_win(user, computer)
+        return "You won!"
+    return "You lost!"
+
