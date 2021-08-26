324908acf330c34a70caa4eb532cc9465f3fdbaa
Fela <felachhkchhk4@gmail.com>
2021-08-26 14:55:54 +0530

    Farenheit and time update

--- Weather_in_Aizawl.py
+++ Weather_in_Aizawl.py
@@ -5,7 +5,14 @@ r = requests.get("https://www.worldweatheronline.com/aizawl-weather/mizoram/in.a
 page_html_str = r.text
 
 inx = page_html_str.index("Aizawl Weather Now")
-temperature = page_html_str[inx+ 427: inx+430]
-print(f"The Temperature in Aizawl is : {temperature}°C")
+inx_date = page_html_str.index("Time in Aizawl is")
+#print(page_html_str[inx_date + 26 : inx_date + 32])
+date = page_html_str[inx_date + 26 : inx_date + 32] + " " + page_html_str[inx_date + 46 : inx_date + 57]
+print(f"\nDate: {date}")
+print("\nThe Temperature in Aizawl: ")
+temp_celcius = page_html_str[inx+ 427: inx+430]
+print(f"In Celcius : {temp_celcius}°C")
+temp_farenheit = (float(temp_celcius) * (9/5)) + 32
+print(f"In Farenheit : {temp_farenheit}°F")
 
 
